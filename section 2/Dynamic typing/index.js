56bb837e978fc15344bc4a5e7ca64f6a9a1a9a2e
Badrun Nahar Luna <100067225+naharluna@users.noreply.github.com>
2026-04-12 18:26:35 +0600

    Dynamic Typing complete

---
+++ section 2/Dynamic typing/index.js
@@ -0,0 +1,15 @@
+// Primitivee/Value Types Types
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. undefined
+// 5. null
+
+let name = 'Luna'; //String Literal
+let age = '25' ; // Number Literal
+let isApprovedtrue = true;  // Boolean Literal
+let isApprovedflase = false; // Boolean Literal
+let firstName = undefined; //undefined Literal
+let lastName = null; //Null used in the situation to clear the variable
+let selectedColor = null; //Null used in the situation to clear the variable
+

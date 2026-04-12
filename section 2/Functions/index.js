bc3c8d5d1e0978941c6f774020a1e46e0e36ef0d
Badrun Nahar Luna <100067225+naharluna@users.noreply.github.com>
2026-04-12 18:51:34 +0600

    Complete Functions

---
+++ section 2/Functions/index.js
@@ -0,0 +1,34 @@
+//Fuctions are fundamental elements in js
+//a set statements that can calculates or performs task a value
+
+function greet(){
+    //body
+    console.log('hello world');
+}
+
+greet();
+
+//input can change the function performs
+//parameter is the at the declaration as name and argument is the actual value we passing as Nahar
+
+function greet1(name){
+    console.log('hello ' + name);
+}
+
+greet1('Badrun')
+greet1('Nahar')
+
+function greet1(name, lastName){
+    console.log('hello ' + name + ' ' + lastName);
+}
+
+greet1('Badrun');
+greet1('Nahar');
+
+function greet1(name, lastName){
+    console.log('hello ' + name + ' ' + lastName);
+}
+
+greet1('Badrun', 'Nahar');
+
+

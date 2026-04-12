16dcef60c41df77c5bce25d9b7b815447daa4e54
Badrun Nahar Luna <100067225+naharluna@users.noreply.github.com>
2026-04-12 22:09:11 +0600

    complete Ternary operator

---
+++ section 3/Equality Operators/index.js
@@ -0,0 +1,9 @@
+
+// Strict Equality operator (same type + same Value)
+console.log(1 === 1);
+console.log('1' === 1);
+
+
+// Loose Equality 
+console.log(1 == 1);
+console.log('1' == 1); //it convert the type according to the left side, and only check the value

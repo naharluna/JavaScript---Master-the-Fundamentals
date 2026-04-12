48bed6081dd0b883a0e6111737f33fa9ae39b38b
Badrun Nahar Luna <100067225+naharluna@users.noreply.github.com>
2026-04-12 18:56:38 +0600

    Section 2 completed

---
+++ section 2/Types of Functions/index.js
@@ -0,0 +1,18 @@
+//Performing a task
+
+function greet(name, lastName){
+    console.log('hello ' + name + ' ' + lastName);
+}
+
+//Calculating a value
+function square(number){
+    return number * number;
+}
+
+let number = square(2);
+
+console.log(number);
+
+console.log(square(2));
+
+

b975d131d81e8669a674fb5eb1342b4ef70e585e
Badrun Nahar Luna <100067225+naharluna@users.noreply.github.com>
2026-04-12 21:26:53 +0600

    Add files via upload

---
+++ section 3/Comparison Operators/index.js
@@ -0,0 +1,14 @@
+// it boolen, true or flase
+
+let x = 1;
+
+// Relational Operators
+console.log( x > 0);
+console.log( x >= 0);
+console.log( x < 0);
+console.log( x <= 0);
+
+
+// Equally
+console.log( x === 1);
+console.log( x !== 1);

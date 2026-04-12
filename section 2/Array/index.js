123699eb38c1a04971eb892a2eb3202cc18a26f8
Badrun Nahar Luna <100067225+naharluna@users.noreply.github.com>
2026-04-12 18:41:15 +0600

    Completed Array

---
+++ section 2/Array/index.js
@@ -0,0 +1,18 @@
+
+let selectedColors = ['red', 'blue']; //[] these are array literal and indicate empty array
+
+console.log(selectedColors);
+
+console.log(selectedColors[0]);
+
+selectedColors[2] = 'green';
+console.log(selectedColors);
+
+
+selectedColors[3] = '1';
+console.log(selectedColors);
+
+
+console.log(selectedColors.length);
+
+

d8020cc88db083b0bc6ee3a97764d27bca70c51e
Badrun Nahar Luna <100067225+naharluna@users.noreply.github.com>
2026-04-12 18:05:02 +0600

    Complete 8.1 Variables

---
+++ section 2/index.js
@@ -0,0 +1,15 @@
+
+let name = 'Luna'; // Declate a variable we use let keyword
+console.log(name);
+
+
+//Vriable names rule:
+//Cannot be a reserved keyword , like if, else, etc
+//Should be meaningful name 
+//Cannot start with a number (1name)
+//cannot contain a space or hyphen (-)
+//They are case-sensitive
+
+let firstname = 'Luna' ;
+let lastname = 'Nahar';
+

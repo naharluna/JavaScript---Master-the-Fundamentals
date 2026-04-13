93980949630eb59595e39e907b5c618524860dec
Badrun Nahar Luna <100067225+naharluna@users.noreply.github.com>
2026-04-13 12:54:40 +0600

    Add files via upload

---
+++ section 4/for/index.js
@@ -0,0 +1,12 @@
+for (let i = 0; i<5; i++) {
+    console.log('Hello WOrlds', i);
+}
+for (let i = 1; i<=5; i++) {
+    if(i % 2 !== 0)
+        console.log(i);
+}
+
+for (let i = 1; i>=5; i--) {
+    if(i % 2 !== 0)
+        console.log(i);
+}
---
+++ section 4/infinite loop/index.js
@@ -0,0 +1,19 @@
+let i = 0;
+while ( i<5){
+    console.log(i);
+    i++;
+}
+
+while(true){
+
+}
+
+let x = 0 ;
+do {
+    //x++;
+}
+while(x <5);
+
+for(let i = 0; i<10; ){
+    
+}

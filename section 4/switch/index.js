55378ea45f91696605d41ff8d1c28e54563d53af
Badrun Nahar Luna <100067225+naharluna@users.noreply.github.com>
2026-04-13 12:43:00 +0600

    Add files via upload

---
+++ section 4/switch/index.js
@@ -0,0 +1,23 @@
+let role = 'guest';
+
+switch (role){
+    case 'guest' : 
+        console.log('Guest User');
+        break;
+
+    case 'moderator':
+        console.log('Moderator User');
+        break;
+
+    default: 
+        console.log('Unknown User');
+}
+
+
+//If-else
+if (role === 'guest')
+    console.log('Guest');
+else if (role === 'moderator')
+    console.log('Moderator');
+else 
+    console.log('Unknown User');
